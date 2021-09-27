20a944316b0de47d3e5b69b6decf851b7d78f5cf
Marcel Rus <marcel.rus.design@gmail.com>
2021-09-26 22:37:33 -0700

    Add ProjectZero component

---
+++ src/components/ProjectZero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ProjectZero.module.scss';
+
+function ProjectZero() {
+    return (
+        <p className={`${styles.topPage}`}>Project Zero</p>
+    );
+}
+
+export default ProjectZero;
