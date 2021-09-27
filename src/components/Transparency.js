977cb26c10284030bff5a7a19ff2090001a21ae9
Marcel Rus <marcel.rus.design@gmail.com>
2021-09-26 23:00:55 -0700

    Add content to Transparency component

--- src/components/Transparency.js
+++ src/components/Transparency.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Section from './Section.js';
+import CallToAction from './CallToAction.js';
 import styles from './Transparency.module.scss';
+
+import transparencyLogo from '../assets/images/amazon/transparecy_logo.png';
+import transparencyVideo from '../assets/images/amazon/transparency.png';
 
 function Transparency() {
     return (
-        <p className={`${styles.topPage}`}>Transparency</p>
+        <div className={styles.topPage}>
+            <Section
+                columnsLeft={"large-6"}
+                columnsMiddle={"large-1"}
+                columnsRight={"large-6"}
+                left={
+                    <CallToAction
+                        image={transparencyLogo}
+                        heading={"Proactively prevent counterfeits"}
+                        paragraph={"Transparency is a product serialization service that helps identify individual units and proactively prevent counterfeits from reaching customers."}
+                        button={"Get started"}
+                    />
+                }
+                right={
+                    <img src={transparencyVideo}/>
+                }
+            />
+        </div>
     );
 }
 
