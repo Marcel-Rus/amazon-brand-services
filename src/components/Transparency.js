b0a0e1e2801d037fbb9c03433c826c3c1d5950ef
Marcel Rus <marcel.rus.design@gmail.com>
2021-09-26 22:37:15 -0700

    Add Transparency component

---
+++ src/components/Transparency.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Transparency.module.scss';
+
+function Transparency() {
+    return (
+        <p className={`${styles.topPage}`}>Transparency</p>
+    );
+}
+
+export default Transparency;
