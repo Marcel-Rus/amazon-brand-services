1b51f7ee8b2788643972ee5168b90853033718e3
Marcel Rus <marcel.rus.design@gmail.com>
2021-09-26 22:37:49 -0700

    Add IPAccelerator component

---
+++ src/components/IPAccelerator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './IPAccelerator.module.scss';
+
+function IPAccelerator() {
+    return (
+        <p className={`${styles.topPage}`}>IP Accelerator</p>
+    );
+}
+
+export default IPAccelerator;
