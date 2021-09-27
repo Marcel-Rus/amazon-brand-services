4a286104f3790cf80d553415e792a9c531e84a85
Marcel Rus <marcel.rus.design@gmail.com>
2021-09-26 23:01:07 -0700

    Add content to IPAccelerator component

--- src/components/IPAccelerator.js
+++ src/components/IPAccelerator.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Section from './Section.js';
+import CallToAction from './CallToAction.js';
 import styles from './IPAccelerator.module.scss';
+
+import ipAcceleratorLogo from '../assets/images/amazon/ip-accelerator-logo.png';
+import ipImage1 from '../assets/images/amazon/ip-image-1.png';
 
 function IPAccelerator() {
     return (
-        <p className={`${styles.topPage}`}>IP Accelerator</p>
+        <div className={styles.topPage}>
+            <Section
+                columnsLeft={"large-7"}
+                columnsMiddle={"large-1"}
+                columnsRight={"large-5"}
+                left={
+                    <CallToAction
+                        image={ipAcceleratorLogo}
+                        heading={"Protect your intellectual property"}
+                        paragraph={"Amazon IP Accelerator helps businesses more quickly obtain intellectual property (IP) rights and brand protection in Amazon’s stores."}
+                        button={"Get started"}
+                    />
+                }
+                right={
+                    <img src={ipImage1}/>
+                }
+            />
+        </div>
     );
 }
 
