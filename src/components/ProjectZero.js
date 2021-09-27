bc792ce42316ec36e04430efd424c2e45c9780ca
Marcel Rus <marcel.rus.design@gmail.com>
2021-09-26 23:01:18 -0700

    Add content to ProjectZero component

--- src/components/ProjectZero.js
+++ src/components/ProjectZero.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Section from './Section.js';
+import CallToAction from './CallToAction.js';
 import styles from './ProjectZero.module.scss';
+
+import amazonProjectZero from '../assets/images/amazon/amazon-project-zero.png';
+import happyBox from '../assets/images/amazon/Prime-JP-07c.png';
 
 function ProjectZero() {
     return (
-        <p className={`${styles.topPage}`}>Project Zero</p>
+        <div className={styles.topPage}>
+            <Section
+                columnsLeft={"large-6"}
+                columnsMiddle={"large-1"}
+                columnsRight={"large-6"}
+                left={
+                    <CallToAction
+                        image={amazonProjectZero}
+                        heading={"Project Zero leverages the combined strengths of Amazon and brands to drive counterfeits to zero"}
+                        paragraph={""}
+                        button={"Get started"}
+                    />
+                }
+                right={
+                    <img src={happyBox}/>
+                }
+            />
+        </div>
     );
 }
 
